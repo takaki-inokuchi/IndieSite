791e6a65d0b9dcd7027055a51a014348b58c0e6f
井ノ口 孝輝 <inokutitakaki@yahoo.co.jp>
2025-10-07 12:48:03 +0900

    test

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  testEnvironment: "jsdom",
+  testEnvironment: "jest-environment-jsdom",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   transform: {
