e0869e920b607184d3584d9544ab3512d70451b1
井ノ口 孝輝 <inokutitakaki@yahoo.co.jp>
2025-10-07 16:49:06 +0900

    test

--- src/components/__test__/Home.test.tsx
+++ src/components/__test__/Home.test.tsx
@@ -1,4 +1,4 @@
-import { render, waitFor, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { Home } from "../../Pages/Home";
 
 globalThis.fetch = jest.fn();
