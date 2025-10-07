65653be7ae49a0af0ad9fb6130711aaa205c6884
井ノ口 孝輝 <inokutitakaki@yahoo.co.jp>
2025-10-07 12:57:07 +0900

    test成功

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,6 +3,6 @@ module.exports = {
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   transform: {
-    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.jest.json" }], // tsconfig指定!
+    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.jest.json" }], // tsconfig指定.
   },
 };
