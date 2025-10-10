e6bdfe57c7fd2fd5dd67882d957d9b65edc89d1c
井ノ口 孝輝 <inokutitakaki@yahoo.co.jp>
2025-10-10 22:52:03 +0900

    修正

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -39,7 +39,7 @@ export const GameCard: React.FC<Props> = ({ game, rank }) => {
             borderRadius="md"
             fontSize="sm"
           >
-            #{rank}位
+            #{rank}
           </Box>
         </Box>
         <Heading as="h3" size="md" mb={2}>
